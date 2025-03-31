c523d76a027c8015b5467700e2ac982d3542f440
b0bu <maclighiche@gmail.com>
2025-03-31 17:35:33 +0100

    add headless sync first deploy

--- after/plugin/sync.lua
+++ after/plugin/sync.lua
@@ -1,10 +1,23 @@
-local SyncCustomChanges = function(path)
-	local after = "after/plugin/"
-	local lua = "lua/custom/plugin/"
-	os.execute("cp -R " .. vim.fn.stdpath("config") .. after .. path .. after)
-	os.execute("cp -R " .. vim.fn.stdpath("config") .. lua .. path .. lua)
+--- after/plugin/sync.lua
+--- sync custom changes in and out of kickstart
+--- cfg "config from git", ctg "config to git"
+local nvrc = vim.fn.stdpath("config")
+local git = "~/dev/neovimrc"
+
+function Sync(paths)
+	setmetatable(paths, { __index = { source = vim.fn.getcwd(), destination = nvrc } })
+	local source, destination = paths[1] or paths.source, paths[2] or paths.destination
+	local after = "/after/plugin"
+	local lua = "/lua/custom/plugins"
+	print("cp -R " .. source .. lua .. "/* " .. destination .. lua)
+	print("cp -R " .. source .. after .. "/* " .. destination .. after)
+	---os.execute("cp -R " .. home .. lua .. eomh .. lua)
 end
 
-vim.keymap.set("n", "<leader>co", function()
-	SyncCustomChanges("~/dev/neovimrc")
+vim.keymap.set("n", "<leader>cfg", function()
+	Sync({ git, nvrc })
+end)
+
+vim.keymap.set("n", "<leader>ctg", function()
+	Sync({ nvrc, git })
 end)

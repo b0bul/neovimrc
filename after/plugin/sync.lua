d5b2e56dbb6c526cff63e913a1b7d19d5c860cf2
b0bu <maclighiche@gmail.com>
2025-03-31 17:47:04 +0100

    extract to Copy fn

--- after/plugin/sync.lua
+++ after/plugin/sync.lua
@@ -4,13 +4,20 @@
 local nvrc = vim.fn.stdpath("config")
 local git = "~/dev/neovimrc"
 
+function Copy(s, d)
+	--- path join
+	local cmd = s .. "/* " .. d
+	print("cp -R " .. cmd)
+end
+
 function Sync(paths)
 	setmetatable(paths, { __index = { source = vim.fn.getcwd(), destination = nvrc } })
 	local source, destination = paths[1] or paths.source, paths[2] or paths.destination
 	local after = "/after/plugin"
 	local lua = "/lua/custom/plugins"
-	print("cp -R " .. source .. lua .. "/* " .. destination .. lua)
-	print("cp -R " .. source .. after .. "/* " .. destination .. after)
+
+	Copy(source .. lua, destination .. lua)
+	Copy(source .. after, destination .. after)
 	---os.execute("cp -R " .. home .. lua .. eomh .. lua)
 end
 

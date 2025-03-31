ec008b9e18b9d93472748b0ba1d08c7e6b9b9f8c
b0bu <maclighiche@gmail.com>
2025-03-31 21:00:11 +0100

    deploy functionality

--- after/plugin/sync.lua
+++ after/plugin/sync.lua
@@ -7,10 +7,11 @@ local git = "~/dev/neovimrc"
 function Copy(s, d)
 	--- path join
 	local cmd = s .. "/* " .. d
-	print("cp -R " .. cmd)
+	os.execute("cp -R " .. cmd)
 end
 
 function Sync(paths)
+	--- default params if none passed
 	setmetatable(paths, { __index = { source = vim.fn.getcwd(), destination = nvrc } })
 	local source, destination = paths[1] or paths.source, paths[2] or paths.destination
 	local after = "/after/plugin"
@@ -18,7 +19,6 @@ function Sync(paths)
 
 	Copy(source .. lua, destination .. lua)
 	Copy(source .. after, destination .. after)
-	---os.execute("cp -R " .. home .. lua .. eomh .. lua)
 end
 
 vim.keymap.set("n", "<leader>cfg", function()

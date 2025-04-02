238d40d954013b05ee0062186b1dbb2166c8de50
b0bu <maclighiche@gmail.com>
2025-04-02 16:58:37 +0100

    update: adding mappings

--- after/plugin/set.lua
+++ after/plugin/set.lua
@@ -1,4 +1,4 @@
-vim.opt.guicursor = ""
+vim.opt.guicursor = ''
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -12,9 +12,9 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
-vim.opt.backup = false 
+vim.opt.backup = false
 
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
@@ -23,8 +23,21 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append '@-@'
 
-vim.opt.colorcolumn = "80"
-vim.g.mapleader = " "
+vim.opt.colorcolumn = '80'
+vim.g.mapleader = ' '
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.opt.clipboard = 'unnamedplus'
+vim.keymap.set('n', '<space>x', ':.lua<CR>')
+vim.keymap.set('v', '<space>x', ':lua<CR>')
+vim.keymap.set('n', '<space>fd', function()
+  require('telescope.builtin').find_files()
+end)
+vim.keymap.set('n', '<space>en', function()
+  require('telescope.builtin').find_files {
+    cwd = vim.fn.stdpath 'config',
+  }
+end)
